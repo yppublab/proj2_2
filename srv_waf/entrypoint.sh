cfc43bcea687b51b96477b385a306124204f79fc
bao-bao-777 <baobao777@mailforspam.com>
2025-10-31 22:44:27 +0300

    commit

--- srv_waf/entrypoint.sh
+++ srv_waf/entrypoint.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 set -eu
 
+ip route del default 2>/dev/null || true
+ip route add default via "$GATEWAY_IP" || true
+
+apk add shadow openssh sudo
+
 
 # Разворачиваем sshd + adm пользователя
 chmod +x /usr/local/bin/adm.sh
 /usr/local/bin/adm.sh
 /usr/sbin/sshd
 
+chmod 666 /dev/stdout /dev/stderr
+chown -R nginx:nginx /data && chmod -R 700 /data
+exec su -s /bin/sh nginx -c "/usr/share/bunkerweb/all-in-one/entrypoint.sh \"$@\""
+
 # Keep container alive
 sleep infinity
+
+
--- srv_waf/start.sh
+++ srv_waf/start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eu
 
 ip route del default 2>/dev/null || true
@@ -6,6 +6,17 @@ ip route add default via "$GATEWAY_IP" || true
 
 apk add shadow openssh sudo
 
+
+# Разворачиваем sshd + adm пользователя
+chmod +x /usr/local/bin/adm.sh
+/usr/local/bin/adm.sh
+/usr/sbin/sshd
+
 chmod 666 /dev/stdout /dev/stderr
 chown -R nginx:nginx /data && chmod -R 700 /data
 exec su -s /bin/sh nginx -c "/usr/share/bunkerweb/all-in-one/entrypoint.sh \"$@\""
+
+# Keep container alive
+sleep infinity
+
+
